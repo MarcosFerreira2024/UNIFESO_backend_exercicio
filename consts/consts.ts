1b50d276372b4f82d9c7b38c5bd5694cce33630f
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-05-27 18:12:11 -0300

    consts

---
+++ consts/consts.ts
@@ -0,0 +1,4 @@
+export const ORDER_SERVICE_PORT = process.env.ORDER_SERVICE_PORT  || 3003;
+export const PRODUCT_SERVICE_PORT = process.env.PRODUCT_SERVICE_PORT  || 3002;
+export const AUTH_SERVICE_PORT = process.env.AUTH_SERVICE_PORT  || 3001;
+export const GATEWAY_PORT = process.env.GATEWAY_PORT  || 3000;
